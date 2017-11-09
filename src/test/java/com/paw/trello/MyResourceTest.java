c9afe0fde86017b77391865848bdfc34b854a61f
Miłosz Szyrner <milosz.szyrner@gmail.com>
2017-11-09 23:45:27 +0100

    Preparation for Heroku part 3

--- src/test/java/com/paw/trello/MyResourceTest.java
+++ src/test/java/com/paw/trello/MyResourceTest.java
@@ -1,19 +1,15 @@
 package com.paw.trello;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-
-import org.glassfish.grizzly.http.server.HttpServer;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import javax.ws.rs.client.WebTarget;
+
 import static org.junit.Assert.assertEquals;
 
 public class MyResourceTest {
 
-    private HttpServer server;
     private WebTarget target;
 
     @Before
