856ddd846cb9ee2433377935edeb126a9184a2aa
Miłosz Szyrner <milosz.szyrner@comarch.pl>
2017-11-23 07:47:53 +0100

    Change main path of Board Resource

--- src/main/java/com/paw/trello/resources/BoardResource.java
+++ src/main/java/com/paw/trello/resources/BoardResource.java
@@ -21,18 +21,17 @@ import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 import java.util.List;
 
-@Path("/")
+@Path("/{id}/boards")
 public class BoardResource {
 
     @GET
-    @Path("{id}/boards")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public List<BoardData> getBoards(@PathParam("id") Long userId) throws SQLException, NamingException {
         return BoardRepository.getInstance().getItems(userId);
     }
 
     @GET
-    @Path("{id}boards/{bId}")
+    @Path("/{bId}")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response getBoard(@PathParam("id") Long userId, @PathParam("bId") Long bId) throws NamingException {
         BoardData boardById = BoardRepository.getInstance().getItem(userId,bId).get(0);
@@ -40,7 +39,6 @@ public class BoardResource {
     }
 
     @POST
-    @Path("{id}/boards")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_HTML)
     public Response createBoard(@PathParam("id") Long userId, @Valid @ValidBoard BoardData board) throws SQLException, NamingException {
@@ -50,7 +48,7 @@ public class BoardResource {
     }
 
     @PUT
-    @Path("{id}/boards/{bId}")
+    @Path("/{bId}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_HTML)
     public Response updateBoard(@PathParam("id") Long userId, @PathParam("bId") Long bId, BoardData board) throws SQLException, NamingException, InvocationTargetException, IllegalAccessException {
@@ -66,7 +64,7 @@ public class BoardResource {
     }
 
     @DELETE
-    @Path("{id}/boards/{bId}")
+    @Path("/{bId}")
     @Produces(MediaType.TEXT_HTML)
     public Response removeBoard(@PathParam("id") Long userId, @PathParam("bId") Long bId) throws NamingException, SQLException {
         if (BoardRepository.getInstance().existItem(userId, bId)) {
@@ -78,14 +76,14 @@ public class BoardResource {
     }
 
     @GET
-    @Path("{id}/boards/{bId}/rolls")
+    @Path("/{bId}/rolls")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public List<LaneData> getRolls(@PathParam("bId") Long bId) throws SQLException, NamingException {
         return LaneRepository.getInstance().getItems(bId);
     }
 
     @GET
-    @Path("{id}/boards/{bId}/rolls/{rId}")
+    @Path("/{bId}/rolls/{rId}")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response getRoll(@PathParam("bId") Long bId, @PathParam("rId") Long rId) throws SQLException, NamingException {
         LaneData laneById = LaneRepository.getInstance().getItem(bId, rId).get(0);
@@ -93,7 +91,7 @@ public class BoardResource {
     }
 
     @POST
-    @Path("{id}/boards/{bId}/rolls")
+    @Path("/{bId}/rolls")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_HTML)
     public Response createrRoll(@PathParam("bId") Long bId, @Valid @ValidLane LaneData roll) throws SQLException, NamingException {
@@ -103,7 +101,7 @@ public class BoardResource {
     }
 
     @PUT
-    @Path("{id}/boards/{bId}/rolls/{rId}")
+    @Path("/{bId}/rolls/{rId}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_HTML)
     public Response updateRoll(@PathParam("bId") Long bId, @PathParam("rId") Long rId, LaneData roll) throws NamingException, SQLException, InvocationTargetException, IllegalAccessException {
@@ -117,7 +115,7 @@ public class BoardResource {
     }
 
     @DELETE
-    @Path("{id}/boards/{bId}/rolls/{rId}")
+    @Path("/{bId}/rolls/{rId}")
     @Produces(MediaType.TEXT_HTML)
     public Response removeRoll(@PathParam("bId") Long bId, @PathParam("rId") Long rId) throws NamingException, SQLException {
         if (LaneRepository.getInstance().existItem(bId, rId)) {
@@ -129,14 +127,14 @@ public class BoardResource {
     }
 
     @GET
-    @Path("{id}/boards/{bId}/rolls/{rId}/cards")
+    @Path("/{bId}/rolls/{rId}/cards")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public List<CardData> getCards(@PathParam("rId") Long rId) throws SQLException, NamingException {
         return CardRepository.getInstance().getItems(rId);
     }
 
     @GET
-    @Path("{id}/boards/{bId}/rolls/{rId}/cards/{cId}")
+    @Path("/{bId}/rolls/{rId}/cards/{cId}")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response getCard(@PathParam("rId") Long rId, @PathParam("cId") Long cId) throws SQLException, NamingException {
         CardData cardById = CardRepository.getInstance().getItem(rId, cId).get(0);
@@ -144,7 +142,7 @@ public class BoardResource {
     }
 
     @POST
-    @Path("{id}/boards/{bId}/rolls/{rId}/cards")
+    @Path("/{bId}/rolls/{rId}/cards")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_HTML)
     public Response createrCard(@PathParam("rId") Long rId, @Valid @ValidCard CardData card) throws SQLException, NamingException {
@@ -154,7 +152,7 @@ public class BoardResource {
     }
 
     @PUT
-    @Path("{id}/boards/{bId}/rolls/{rId}/cards/{cId}")
+    @Path("/{bId}/rolls/{rId}/cards/{cId}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_HTML)
     public Response updateCard(@PathParam("rId") Long rId, @PathParam("cId") Long cId, CardData card) throws NamingException, SQLException, InvocationTargetException, IllegalAccessException {
@@ -168,7 +166,7 @@ public class BoardResource {
     }
 
     @DELETE
-    @Path("{id}/boards/{bId}/rolls/{rId}/cards/{cId}")
+    @Path("/{bId}/rolls/{rId}/cards/{cId}")
     @Produces(MediaType.TEXT_HTML)
     public Response removeCard(@PathParam("rId") Long rId, @PathParam("cId") Long cId) throws NamingException, SQLException {
         if (CardRepository.getInstance().existItem(rId, cId)) {
@@ -180,14 +178,14 @@ public class BoardResource {
     }
 
     @GET
-    @Path("{id}/boards/{bId}/rolls/{rId}/cards/{cId}/remarks")
+    @Path("/{bId}/rolls/{rId}/cards/{cId}/remarks")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public List<RemarkData> getRemarks(@PathParam("cId") Long cId) throws SQLException, NamingException {
         return RemarkRepository.getInstance().getItems(cId);
     }
 
     @GET
-    @Path("{id}/boards/{bId}/rolls/{rId}/cards/{cId}/remarks/{rmId}")
+    @Path("/{bId}/rolls/{rId}/cards/{cId}/remarks/{rmId}")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response getRemark(@PathParam("cId") Long cId, @PathParam("rmId") Long rmId) throws SQLException, NamingException {
         RemarkData remarkById = RemarkRepository.getInstance().getItem(cId, rmId).get(0);
@@ -195,7 +193,7 @@ public class BoardResource {
     }
 
     @POST
-    @Path("{id}/boards/{bId}/rolls/{rId}/cards/{cId}/remarks")
+    @Path("/{bId}/rolls/{rId}/cards/{cId}/remarks")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_HTML)
     public Response createRemark(@PathParam("cId") Long cId, @Valid @ValidRemark RemarkData remark) throws SQLException, NamingException {
@@ -205,7 +203,7 @@ public class BoardResource {
     }
 
     @PUT
-    @Path("{id}/boards/{bId}/rolls/{rId}/cards/{cId}/remarks/{rmId}")
+    @Path("/{bId}/rolls/{rId}/cards/{cId}/remarks/{rmId}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_HTML)
     public Response updateRemark(@PathParam("cId") Long cId, @PathParam("rmId") Long rmId, RemarkData remark) throws NamingException, SQLException, InvocationTargetException, IllegalAccessException {
@@ -219,7 +217,7 @@ public class BoardResource {
     }
 
     @DELETE
-    @Path("{id}/boards/{bId}/rolls/{rId}/cards/{cId}/remarks/{rmId}")
+    @Path("/{bId}/rolls/{rId}/cards/{cId}/remarks/{rmId}")
     @Produces(MediaType.TEXT_HTML)
     public Response removeRemark(@PathParam("cId") Long cId, @PathParam("rmId") Long rmId) throws NamingException, SQLException {
         if (RemarkRepository.getInstance().existItem(cId, rmId)) {
